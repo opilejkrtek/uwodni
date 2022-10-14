2354935930673dbe5170c2aa699f187d8f398387
opilejkrtek <100448631+opilejkrtek@users.noreply.github.com>
2022-10-15 00:06:59 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+def main():
+    import time
+    import random
+    y = int (input("dej sem číslo uwu: "))
+    x = random.randint (1,10)
+    z = x - y
+    if y > 10 or y < 1:
+        if y == 69 or y == 420 or y == 69420:
+            print("haha velice vtipné.... xd")
+        else:
+            print("musí to být jedna až deset ty ťunťo... ")
+    else:
+        if x == y:
+          print("správně, šikula :) záhadné číslo bylo", x ,"(což je jakože to stejný žejo xd)")
+        else:
+             print("snad příště :<,záhadné číslo bylo",x,"...takže jsi se netrefil o", abs(z), "uwu" )
+    print("-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~")
+
+    retard_teda_chtěljsemříct_restart=input ("napiš ano pokud chceš zkusit znovu(pokud napíšeš jj tak to bude fungovat taky xddd): ")
+    if retard_teda_chtěljsemříct_restart == "ano" or retard_teda_chtěljsemříct_restart == "jj":
+        main()
+
+    else:
+       print ("by imagine and vatashiii")
+       input("press enter to exit (=^ w ^=)")
+
+main()
+#epicke velice uwu mommy dommy anime gf
